Corrected parametric space T and S assignments and created a gradient testing script

main.py
@@ -75,14 +75,13 @@ Pk_in[ind_tin]= tin
 #----------------Step 2------------------------------------------------
 # first guess of the s-t space
 grid_s, grid_t= np.mgrid[0:N_s, 0:N_c]
-
 #------------test for Q generation---------------------------------------------
 #S= np.zeros((10, 1), dtype= float) #spanwise section
 #S.fill(1)
 #T= np.zeros((10, 1), dtype= float) #chordwise section
 #T[0:, 0]= np.arange(0, 10)
 #----------------------------------------------------------------------------
-for i in range(Ns_desired):
+for i in range(500,501):#(Ns_desired):
   #flag for exiting the while loop
   exit_flag= 1
   # store initial zc 
@@ -121,10 +120,10 @@ for i in range(Ns_desired):
 # calculate the analytic gradients of each stage
 
 # jacobian as a sparse matrix for Q-->(x,y,z) wrt P-->(s,t) of size 3Nx2N
-    jac_qp, dZds, dZdt= jacobian_Q(S, T, grid_map, val_map)
+    jac_qp, _, _, _, _, dZds, dZdt= jacobian_Q(S, T, grid_map, val_map)
 
 # jacobian as a sparse matrix for D-->(di) wrt Q-->(x,y,z) of size Nx3N
-    jac_dq= jacobian_D(Q, D)
+    jac_dq, _, _, _, _, _, _= jacobian_D(Q, D)
 
 # jacobian as a sparse matrix for D-->(di) wrt P-->(s,t) of size Nx2N
     jac_dp= jac_dq*jac_qp

parametric_space.py
@@ -26,12 +26,32 @@ def bilinear_surface(surface_orig, grid_s, grid_t, S, T):
 # interpolant grid location
    grid_map= np.empty((Ncs_desired), dtype= object)    
    val_map= np.empty((Ncs_desired), dtype= object)
-         
+   
+#------------------treatment for S and/or T exceeding the bound-space---------
+   Ns= S.shape[0]
+   S[S<0]= 0
+   S[S>=Ns]= Ns-1
+   
+   if np.min(T)< 0:
+       T_low= T[T<0]
+       quot= abs(T_low)/Ncs_desired
+       quot= quot.astype(int)
+       rem= abs(T_low) - quot*Ncs_desired
+       T[T<0]= rem
+       
+   if np.max(T)>= Ncs_desired:
+       T_high= T[T>=Ncs_desired]
+       quot= abs(T_high)/Ncs_desired
+       quot= quot.astype(int)
+       rem= abs(T_high) - quot*Ncs_desired
+       T[T>=Ncs_desired]= (Ncs_desired-1) - rem
+#----------------------------------------------------------------------------      
+   
    for i in range(Ncs_desired):
     #store the x-coordinate of the desired point
-      x= T[i]
+      x= S[i]
     #store the y-coordinate of the desired point
-      y= S[i]
+      y= T[i]
     #obtain the closest index of the x-coordinate in the original grid
       idx= (np.abs(x - grid_s)).argmin(axis=0)[0]
     # obtain the closest index of the y-coordinate in the desired grid
@@ -49,8 +69,8 @@ def bilinear_surface(surface_orig, grid_s, grid_t, S, T):
       indices_y[Py1]= [idx, idy]
         
       # obtain the neighbourhood
-      up_bound_x= np.max(T)
-      up_bound_y= np.max(S) 
+      up_bound_x= np.max(grid_s)
+      up_bound_y= np.max(grid_t) 
       
       # obtain the second y-coordinate
       if Py1 == up_bound_y or y< Py1:
@@ -61,7 +81,7 @@ def bilinear_surface(surface_orig, grid_s, grid_t, S, T):
                 
       else:
          Py2 = grid_t[idx, idy + 1]
-     
+         
          indices_y[Py2]= [idx, idy + 1]       
       # obtain the second x-coordinate
       if Px1 == up_bound_x or x< Px1:
@@ -70,7 +90,9 @@ def bilinear_surface(surface_orig, grid_s, grid_t, S, T):
           
          indices_x[Px2]= [idx - 1, idy]
       else:
-         Px2 = grid_s[idx + 1, idy] 
+         #print('(Px1=%i, Py1=%i), (Px2=%i, Py2=%i)'%(Px1, Py1, Px2, Py2)) 
+         Px2 = grid_s[idx + 1, idy]
+         
             
          indices_x[Px2]= [idx + 1, idy]
     # sort the neighbourhood in ascending order
@@ -126,8 +148,8 @@ def bilinear_surface(surface_orig, grid_s, grid_t, S, T):
         #Coefficient matrix for Z-values
        Bz= np.linalg.inv(A) * Z
         
-       x_desired= T[i]
-       y_desired= S[i]
+       x_desired= S[i]
+       y_desired= T[i]
         
         # X-value for the new surface
        Q[i, 0]= (float(Bx[0]) + float(Bx[1])*x_desired + 

vector_operations.py
@@ -210,7 +210,75 @@ def jacobian_Q(S, T, grid_map, val_map):
     #store the jacobian in the COO sparse format
     jac_q= coo_matrix((data,(row, col)), shape= (3*n_points, 2*n_points))
     
-    return jac_q, dZds, dZdt
+    return jac_q, dXds, dXdt, dYds, dYdt, dZds, dZdt
+
+def test_jacobian_Q(Qin, Qf_s, Qf_t, h, Ns_desired):
+    
+    grad_Qs= (Qf_s-Qin)/h
+    grad_Qt= (Qf_t-Qin)/h
+    
+    n_points= Ns_desired
+    
+    #total sizeof the jacobian
+    #size= 2*3*(n_points**2)
+    
+    # store dxds, dxdt, dyds, dydt, dzds, dzdt in that order
+    # for every 1 point there will be 6 non-zero partials, for N --> 6*N
+    data= np.zeros(6*n_points, dtype= float)
+    row= np.zeros(6*n_points, dtype= int)
+    col= np.zeros(6*n_points, dtype= int)
+    
+    # inititalize the row andcolumn indices for the partials being stored
+    #row_x= 0
+    #row_y= 1
+    #row_z= 2
+    #col_s= 0
+    #col_t= 1
+    
+    #initialize the numpy arrays for the partials
+    dXds= grad_Qs[:,0]
+    dXdt= grad_Qt[:,0]
+    dYds= grad_Qs[:,1]
+    dYdt= grad_Qt[:,1]
+    dZds= grad_Qs[:,2]
+    dZdt= grad_Qt[:,2]
+    ind= np.arange(0, 6*n_points, 6)
+    
+    #store the partial derivatives in the data vector
+    data[ind]= dXds
+    data[ind+1]= dXdt
+    data[ind+2]= dYds
+    data[ind+3]= dYdt
+    data[ind+4]= dZds
+    data[ind+5]= dZdt
+    
+    #store the row indices of corresponding partials
+    row_x= np.arange(0, 3*n_points, 3)
+    row_y= np.arange(1, 3*n_points, 3)
+    row_z= np.arange(2, 3*n_points, 3)
+    
+    row[ind]= row_x
+    row[ind+1]= row_x
+    row[ind+2]= row_y
+    row[ind+3]= row_y
+    row[ind+4]= row_z
+    row[ind+5]= row_z
+    
+    #store the coloumn indices of corrsponding partials
+    col_s= np.arange(0, 2*n_points, 2)
+    col_t= np.arange(1, 2*n_points, 2)
+        
+    col[ind]= col_s
+    col[ind+1]= col_t
+    col[ind+2]= col_s
+    col[ind+3]= col_t
+    col[ind+4]= col_s
+    col[ind+5]= col_t
+    
+    #store the jacobian in the COO sparse format
+    jac_qp_test= coo_matrix((data,(row, col)), shape= (3*n_points, 2*n_points))
+    
+    return jac_qp_test, dXds, dXdt, dYds, dYdt, dZds, dZdt
 
 def jacobian_D(Q, D):
     """ Stores the jacobian of the distances between consecutive points on the
@@ -336,7 +404,73 @@ def jacobian_D(Q, D):
     
     jac_d= coo_matrix((data,(row, col)), shape= (n_points, 3*n_points))
     
-    return jac_d
+    return jac_d, dDdX1, dDdX2, dDdY1, dDdY2, dDdZ1, dDdZ2
+
+def test_jacobian_D(D, D_x1_h, D_x2_h, D_y1_h, D_y2_h, D_z1_h, D_z2_h, h):
+        
+    # calculate the partials
+    dDdX1= (D_x1_h - D)/h
+    dDdX2= (D_x2_h - D)/h
+    
+    dDdY1= (D_y1_h - D)/h
+    dDdY2= (D_y2_h - D)/h
+    
+    dDdZ1= (D_z1_h - D)/h
+    dDdZ2= (D_z2_h - D)/h
+    
+    # number of points on the slice
+    n_points= D.shape[0]
+    
+    #total sizeof the jacobian
+   # size= 3*(n_points**2)
+    # store dxds, dxdt, dyds, dydt, dzds, dzdt in that order
+    # for every 1 distance, there will be 6 non-zero partials
+    data= np.zeros(6*n_points, dtype= float)
+    row= np.zeros(6*n_points, dtype= int)
+    col= np.zeros(6*n_points, dtype= int)
+    
+        
+    #store the partial derivatives
+    #TODO without for loop
+    ind= np.arange(0, 6*n_points, 6)
+    
+    #store the partial derivatives in the data vector
+    data[ind]= dDdX1
+    data[ind+1]= dDdY1
+    data[ind+2]= dDdZ1
+    data[ind+3]= dDdX2
+    data[ind+4]= dDdY2
+    data[ind+5]= dDdZ2
+    
+    #store the row indices of corresponding partials
+    row_d= np.arange(0, n_points, 1)
+    row[ind]= row_d
+    row[ind+1]= row_d
+    row[ind+2]= row_d
+    row[ind+3]= row_d
+    row[ind+4]= row_d
+    row[ind+5]= row_d
+    
+    col_x= np.arange(0, 3*n_points, 3)
+    col_y= np.arange(1, 3*n_points, 3)
+    col_z= np.arange(2, 3*n_points, 3)
+    
+    col[ind]= col_x
+    col[ind+1]= col_y
+    col[ind+2]= col_z
+    col[ind+3]= col_x + 3
+    col[ind+4]= col_y + 3
+    col[ind+5]= col_z + 3
+    #overwriting the final three indices to close the loop
+    col[-1]= 2 #del(dn)/del(z1)
+    col[-2]= 1 #del(dn)/del(z2)
+    col[-3]= 0 #del(dn)/del(z3)
+    
+    jac_dq_test= coo_matrix((data,(row, col)), shape= (n_points, 3*n_points))
+    
+    return jac_dq_test, dDdX1, dDdX2, dDdY1, dDdY2, dDdZ1, dDdZ2
+
+def test_jacobian_DP():
 
 def jacobian_main(dZds, dZdt, jac_dp, n_points):
     """ Constructs the main jacobian matrix of size 2N x (2N+1)