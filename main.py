Modularized testing script for Jac(D/p), tested jacobians and fixed assignment of jac(dc)

main.py
@@ -13,11 +13,10 @@ Created on Thu Dec  7 16:58:33 2017
 """
 import time
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D
+#from matplotlib import pyplot as plt
 from parametric_space import bilinear_surface #user-defined method to perform bilinear
-                                         #grid interpolation
-#from generate_loftedblade import kb6_loftedblade
+                                              #grid interpolation
 import pickling #user-defined module to store and load python data as pickles
 from vector_operations import calculate_distance
 from vector_operations import jacobian_Q
@@ -44,8 +43,9 @@ N_c= 1000
 N_s= 1000
 #generate the lofted surface
 #surface = kb6_loftedblade(optimization_file, iteration, N_c, N_s)
+blade_length= 10.55 # in metres
 surface= pickling.load_obj('KB6_surf_1000by1000') 
- 
+surface= surface*blade_length 
 #initialize the residual vector constants
 dc_in= 0 # distance constant that is dtermined by Newton method
 
@@ -60,7 +60,7 @@ n_points= Nc_desired
 
 surface_new= np.zeros((Nc_desired, Ns_desired, 3), dtype= float)
 #set value of zc
-zc_vec= np.linspace(0, 1, Ns_desired)
+zc_vec= np.linspace(0, 1*blade_length, Ns_desired)
 
 #initialize the Pk vector with s and t points
 Pk_in= np.zeros(2*n_points+1, dtype=float)
@@ -98,63 +98,58 @@ for i in range(500,501):#(Ns_desired):
   #counter
   count=0 
   while exit_flag:
-#-------------------------------------------------------------------
-# obtain the X,Y,Z points for the S and T vectors
-# Q[N, 3] where N=number of points in the slice
-    
-      
-      
+  #-------------------------------------------------------------------
+    # obtain the X,Y,Z points for the S and T vectors 
+    # Q[N, 3] where N=number of points in the slice
     S= Pk[ind_sin] 
     T= Pk[ind_tin]
     
     Q, grid_map, val_map= bilinear_surface(surface, grid_s, grid_t, S, T)
-#----------------------------------------------------------------------------
-#---------------------------------------------------------------------------
-
-#------------------------Step 3---------------------------------------------
-#calculate distance between consecutive x,y,z in the slice also add the final 
-#point and first point to close the loop
+    #----------------------------------------------------------------------------
+    #------------------------Step 3---------------------------------------------
+    #calculate distance between consecutive x,y,z in the slice also add the final 
+    #point and first point to close the loop
     D= calculate_distance(Q[:, 0], Q[:, 1], Q[:, 2], flag= True)
 
-#------------------------Step 4---------------------------------------------
-# calculate the analytic gradients of each stage
+    #------------------------Step 4---------------------------------------------
+    # calculate the analytic gradients of each stage
 
-# jacobian as a sparse matrix for Q-->(x,y,z) wrt P-->(s,t) of size 3Nx2N
+    # jacobian as a sparse matrix for Q-->(x,y,z) wrt P-->(s,t) of size 3Nx2N
     jac_qp, _, _, _, _, dZds, dZdt= jacobian_Q(S, T, grid_map, val_map)
 
-# jacobian as a sparse matrix for D-->(di) wrt Q-->(x,y,z) of size Nx3N
+    # jacobian as a sparse matrix for D-->(di) wrt Q-->(x,y,z) of size Nx3N
     jac_dq, _, _, _, _, _, _= jacobian_D(Q, D)
 
-# jacobian as a sparse matrix for D-->(di) wrt P-->(s,t) of size Nx2N
+    # jacobian as a sparse matrix for D-->(di) wrt P-->(s,t) of size Nx2N
     jac_dp= jac_dq*jac_qp
 
-# construct the final jacobian matrix of order (2N+1)x(2N+1) with d-dc, z-zc, t-tc
-#partials
+    # construct the final jacobian matrix of order (2N+1)x(2N+1) with d-dc, z-zc, t-tc
+    # partials
     n_points= S.shape[0] # number of slice points
 
     jac_main= jacobian_main(dZds, dZdt, jac_dp, n_points)
 
-#------------------Step 5------------------------------------------------
-#Newton Rhapson solver
+    #------------------Step 5------------------------------------------------
+    #Newton Rhapson solver
 
-#construct the residual vector
-#NOTE: for the np.arraysuse np.dot for matrix multiplication where column vectors
-# and row vectors are automatically treated.
+    #construct the residual vector
+    #NOTE: for the np.arrays use np.dot for matrix multiplication where column vectors
+    # and row vectors are automatically treated.
     R= np.zeros((2*n_points + 1), dtype=float)
-
-# fill up the distance function di-dc
+ 
+    # fill up the distance function di-dc
     #update dc
     dc= Pk[-1]
     R[:n_points]= D - dc
-#fill up the z coordinate function z-zc
+    #fill up the z coordinate function z-zc
     R[n_points:2*n_points]= Q[:, 2] - zc
-#fill up the t1-tc function 
+    #fill up the t1-tc function 
     R[-1]= T[0] - tc
 
 
-#-------------------Step 6--------------------------------------------------
-# add a check to exit the newton method
-# ex: np.max(R)<1e-5 
+    #-------------------Step 6--------------------------------------------------
+    # add a check to exit the newton method
+    # ex: np.max(R)<1e-5 
     if np.max(R)<1e-5:
         # set exit flag as False
         exit_flag= 0
@@ -164,16 +159,18 @@ for i in range(500,501):#(Ns_desired):
         surface_new[:, i, 2]= Q[:, 2]
         break
     
-#-----------------Step 7---------------------------------------------------
-# store the k+1 values of the P vector
-# P = [s1, t1, s2, t2...si,ti...., dc] order: (2N+1) x 1
+    #-----------------Step 7---------------------------------------------------
+    #inverse the main jacobain array
     jac_main_inv= np.linalg.pinv(jac_main.toarray())
-    
+    # store the k+1 values of the P vector
+    # P = [s1, t1, s2, t2...si,ti...., dc] order: (2N+1) x 1
     Pk1= Pk - np.dot(jac_main_inv, R) 
-    
+    #update P
     Pk=Pk1
-
+    #increase count
     count+=1  
+    
+    
 # check grid
 #fig = plt.figure()
 #ax = fig.add_subplot(111, projection='3d')

testing_script.py
@@ -20,7 +20,7 @@ from vector_operations import test_jacobian_Q
 from vector_operations import jacobian_D
 from vector_operations import test_jacobian_D
 from vector_operations import jacobian_main
-
+from vector_operations import test_jacobian_DP
 
 #format of surface_orig is [i, j, k] where i= Number of cross-sections,
 # j= Number of spanwise sections, k= (x, y, z)
@@ -172,18 +172,8 @@ jac_dp= jac_dq*jac_qp
 
 #--------------jacobain of D wrt P test obtained numerically------------------
 #jac_dp_test= jac_dq_test*jac_qp_test
-x_s1_h= Qf_s[:, 0]
-x_s2= np.append(Q[1:, 0], Q[0, 0])
-y_s1_h= Qf_s[:, 1]
-y_s2= np.append(Q[1:, 1], Q[0, 1])
-z_s1_h= Qf_s[:, 0]
-z_s2= np.append(Q[1:, 2], Q[0, 2])
 
-D_s1= np.power(np.power(x_s2 - x_s1_h, 2) + np.power(y_s2 - y_s1_h, 2) + 
-      np.power(z_s2 - z_s1_h, 2), 0.5)
-
-D_s2= 
-
+jac_dp_test, dDds1, dDds2, dDdt1, dDdt2= test_jacobian_DP(Q, Qf_s, Qf_t, D, h)
 #-------------------------------------------------------------------------- 
 # construct the final jacobian matrix of order (2N+1)x(2N+1) with d-dc, z-zc, t-tc
 #partials

vector_operations.py
@@ -470,7 +470,86 @@ def test_jacobian_D(D, D_x1_h, D_x2_h, D_y1_h, D_y2_h, D_z1_h, D_z2_h, h):
     
     return jac_dq_test, dDdX1, dDdX2, dDdY1, dDdY2, dDdZ1, dDdZ2
 
-def test_jacobian_DP():
+def test_jacobian_DP(Q, Qf_s, Qf_t, D, h):
+    
+    # store the Q values by perturbing s+h,t 
+    x_s_h= np.append(Qf_s[:, 0], Qf_s[0, 0])
+    x_s= np.append(Q[:, 0], Q[0, 0])
+    y_s_h= np.append(Qf_s[:, 1], Qf_s[0, 1])
+    y_s= np.append(Q[:, 1], Q[0, 1])
+    z_s_h= np.append(Qf_s[:, 2], Qf_s[0, 2])
+    z_s= np.append(Q[:, 2], Q[0, 2])
+    
+    # store the Q values by perturbing s,t+h
+    x_t_h= np.append(Qf_t[:, 0], Qf_t[0, 0])
+    x_t= x_s
+    y_t_h= np.append(Qf_t[:, 1], Qf_t[0, 1])
+    y_t= y_s
+    z_t_h= np.append(Qf_t[:, 2], Qf_t[0, 2])
+    z_t= z_s
+
+    # caclulate distances for Di(s1+h, s2, t1, t2)
+    D_s1= np.power(np.power(x_s[1:] - x_s_h[:-1], 2) + np.power(y_s[1:] - 
+                   y_s_h[:-1], 2) + np.power(z_s[1:] - z_s_h[:-1], 2), 0.5)
+    
+    # caclulate distances for Di(s1, s2+h, t1, t2)
+    D_s2= np.power(np.power(x_s_h[1:] - x_s[:-1], 2) + np.power(y_s_h[1:] - 
+                   y_s[:-1], 2) + np.power(z_s_h[1:] - z_s[:-1], 2), 0.5)
+    
+    # caclulate distances for Di(s1, s2, t1+h, t2)
+    D_t1= np.power(np.power(x_t[1:] - x_t_h[:-1], 2) + np.power(y_t[1:] - 
+                   y_t_h[:-1], 2) + np.power(z_t[1:] - z_t_h[:-1], 2), 0.5)
+    
+    # caclulate distances for Di(s1, s2, t1, t2+h)
+    D_t2= np.power(np.power(x_t_h[1:] - x_t[:-1], 2) + np.power(y_t_h[1:] - 
+                   y_t[:-1], 2) + np.power(z_t_h[1:] - z_t[:-1], 2), 0.5)
+    
+    # gradient D wrt s1
+    dDds1= (D_s1 - D)/h
+    # gradient D wrt s2
+    dDds2= (D_s2 - D)/h
+    # gradient D wrt t1
+    dDdt1= (D_t1 - D)/h
+    # gradient D wrt t2
+    dDdt2= (D_t2 - D)/h
+    
+    # number of points on the slice   
+    n_points= D.shape[0]
+    #initialize nump arrays for the sparese matric data storage
+    data= np.zeros(4*n_points, dtype= float)
+    row= np.zeros(4*n_points, dtype= int)
+    col= np.zeros(4*n_points, dtype= int)
+    # indices for storing the data
+    ind= np.arange(0, 4*n_points, 4)
+    #store the data
+    data[ind]= dDds1
+    data[ind+1]= dDdt1
+    data[ind+2]= dDds2
+    data[ind+3]= dDdt2
+    #define the row and column indices to store the row and column info
+    row_ind= np.arange(0, n_points)
+    row[ind]= row_ind 
+    row[ind+1]= row_ind
+    row[ind+2]= row_ind
+    row[ind+3]= row_ind
+    # assign the column indices
+    col_ind_s1= np.arange(0, 2*n_points, 2) 
+    col_ind_t1= col_ind_s1 + 1
+    col_ind_s2= col_ind_s1 + 2
+    col_ind_t2= col_ind_s1 + 3
+    #adjust the last line to reflect a loop
+    col_ind_s2[-1]= 0
+    col_ind_t2[-1]= 1
+    #store the column indices
+    col[ind]= col_ind_s1
+    col[ind+1]= col_ind_t1
+    col[ind+2]= col_ind_s2
+    col[ind+3]= col_ind_t2
+    
+    #build the sparse jacobian
+    jac_dp_test= coo_matrix((data,(row, col)), shape= (n_points, 2*n_points))
+    
+    return jac_dp_test, dDds1, dDds2, dDdt1, dDdt2
 
 def jacobian_main(dZds, dZdt, jac_dp, n_points):
     """ Constructs the main jacobian matrix of size 2N x (2N+1)
@@ -520,7 +599,8 @@ def jacobian_main(dZds, dZdt, jac_dp, n_points):
     # fill in data
     data_dc[:]= -1
     # fill in row indices
-    row_dc[:]= row_ind_dzdp
+    row_ind_dDdp= np.arange(0, n_points)
+    row_dc= row_ind_dDdp
     # fill in column indices
     col_dc[:]= 2*n_points
     # append it to the main jacobian row, column and data